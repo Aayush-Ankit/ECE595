116d936c2ee4fbf081d5889ae9ebfc73e6256688
Sanchari Sen <sen9@descartes.ecn.purdue.edu>
2017-04-25 19:13:58 -0400

    ran var conv features multiple times to see if it changes

--- mnist/train_lenet_trials_var_featuresize.sh
+++ mnist/train_lenet_trials_var_featuresize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh 
 set -e
-./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize10.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize10_trace2.txt
-./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize25.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize25_trace2.txt
-./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize50.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize50_trace2.txt
-./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize100.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize100_trace2.txt
-./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize150.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize150_trace2.txt
+./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize10.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize10_trace3.txt
+./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize25.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize25_trace3.txt
+./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize50.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize50_trace3.txt
+./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize100.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize100_trace3.txt
+./build/tools/caffe train --solver=ECE595/mnist/solver/var_num_feature_maxiter100/lenet_solver_featuresize150.prototxt $@ 2>&1 | tee ECE595/mnist/traces/var_num_feature_maxiter100/solver_featuresize150_trace3.txt
