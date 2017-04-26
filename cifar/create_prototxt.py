910ace160b93a1455a9cd6d1bd6341241f6f173f
AAYUSH ANKIT <aankit@purdue.edu>
2017-04-26 11:50:02 -0400

    CIFAR - Vary Conv Features

--- cifar/create_prototxt.py
+++ cifar/create_prototxt.py
@@ -1,10 +1,7 @@
-num_trials = 10
-#path = './train_test/var_ker_size/'
-#path = './train_test/var_fc_size/'
-path = './train_test/var_num_feature_maxiter6000/'
+path = './train_test/var_num_feature_maxiter20000/'
 
 # Fucntions to generate layer specific text
-common_text_start = "name: \"CIFARLeNet\"\n\
+common_text_start = "name: \"CIFAR\"\n\
 layer {\n\
   name: \"cifar\"\n\
   type: \"Data\"\n\
@@ -18,7 +15,7 @@ layer {\n\
   }\n\
   data_param {\n\
     source: \"examples/cifar10/cifar10_train_lmdb\"\n\
-    batch_size: 64\n\
+    batch_size: 111\n\
     backend: LMDB\n\
   }\n\
 }\n\
@@ -35,86 +32,10 @@ layer {\n\
   }\n\
   data_param {\n\
     source: \"examples/cifar10/cifar10_test_lmdb\"\n\
-    batch_size: 100\n\
+    batch_size: 1000\n\
     backend: LMDB\n\
   }\n\
 }\n"
-
-def generate_conv_end_text(num_conv_layers):
-    if (num_conv_layers == 0):
-        text1 = "layer {\n\
-  name: \"ip1\"\n\
-  type: \"InnerProduct\"\n\
-  bottom: \"data\"\n"
-    else:
-        text1 = "layer {\n\
-  name: \"ip1\"\n\
-  type: \"InnerProduct\"\n\
-  bottom: \"pool" + str(num_conv_layers-1) + "\"\n"
-      
-        
-    common_text_end = text1 + "  top: \"ip1\"\n\
-  param {\n\
-    lr_mult: 1\n\
-  }\n\
-  param {\n\
-    lr_mult: 2\n\
-  }\n\
-  inner_product_param {\n\
-    num_output: 500\n\
-    weight_filler {\n\
-      type: \"xavier\"\n\
-    }\n\
-    bias_filler {\n\
-      type: \"constant\"\n\
-    }\n\
-  }\n\
-}\n\
-layer {\n\
-  name: \"relu1\"\n\
-  type: \"ReLU\"\n\
-  bottom: \"ip1\"\n\
-  top: \"ip1\"\n\
-}\n\
-layer {\n\
-  name: \"ip2\"\n\
-  type: \"InnerProduct\"\n\
-  bottom: \"ip1\"\n\
-  top: \"ip2\"\n\
-  param {\n\
-    lr_mult: 1\n\
-  }\n\
-  param {\n\
-    lr_mult: 2\n\
-    }\n\
-  inner_product_param {\n\
-    num_output: 10\n\
-    weight_filler {\n\
-      type: \"xavier\"\n\
-    }\n\
-    bias_filler {\n\
-      type: \"constant\"\n\
-    }\n\
-  }\n\
-}\n\
-layer {\n\
-  name: \"accuracy\"\n\
-  type: \"Accuracy\"\n\
-  bottom: \"ip2\"\n\
-  bottom: \"label\"\n\
-  top: \"accuracy\"\n\
-  include {\n\
-    phase: TEST\n\
-  }\n\
-}\n\
-layer {\n\
-  name: \"loss\"\n\
-  type: \"SoftmaxWithLoss\"\n\
-  bottom: \"ip2\"\n\
-  bottom: \"label\"\n\
-  top: \"loss\"\n\
- }\n"
-    return common_text_end
 
 def generate_conv_text(layer_id, kernel_size, num_features):
     if (layer_id == 0):
@@ -137,10 +58,12 @@ def generate_conv_text(layer_id, kernel_size, num_features):
   }\n\
   convolution_param {\n\
     num_output: " + str(num_features) + "\n\
+    pad: 2\n\
     kernel_size: " + str(kernel_size) + "\n\
     stride: 1\n\
     weight_filler {\n\
-      type: \"xavier\"\n\
+      type: \"gaussian\"\n\
+      std: 0.01\n\
     }\n\
     bias_filler {\n\
       type: \"constant\"\n\
@@ -149,21 +72,37 @@ def generate_conv_text(layer_id, kernel_size, num_features):
 }\n"
     return text
 
+def generate_sigmoid_text(layer_id):
+    text = "layer {\n\
+  name: \"Sigmoid" + str(layer_id) + "\"\n\
+  type: \"Sigmoid\"\n\
+  bottom: \"conv" + str(layer_id) + "\"\n\
+  top: \"Sigmoid" + str(layer_id) + "\"\n\
+}\n"
+    return text
 
 def generate_pool_text(layer_id):
     text = "layer {\n\
   name: \"pool" + str(layer_id) + "\"\n\
   type: \"Pooling\"\n\
-  bottom: \"conv" + str(layer_id) + "\"\n\
+  bottom: \"Sigmoid" + str(layer_id) + "\"\n\
   top: \"pool" + str(layer_id) + "\"\n\
   pooling_param {\n\
-    pool: MAX\n\
-    kernel_size: 2\n\
+    pool: AVE\n\
+    kernel_size: 3\n\
     stride: 2\n\
   }\n\
 }\n"
     return text
 
+def generate_relu_text(layer_id):
+    text = "layer {\n\
+  name: \"relu" + str(layer_id) + "\"\n\
+  type: \"relu\"\n\
+  bottom: \"ip" + str(layer_id) + "\"\n\
+  top: \"ip" + str(layer_id) + "\"\n\
+}\n"
+    return text
 
 def generate_fc_text(layer_id, num_output, num_conv):
     if (layer_id == 1):
@@ -188,7 +127,8 @@ def generate_fc_text(layer_id, num_output, num_conv):
   inner_product_param {\n\
     num_output: " + str(num_output) + "\n\
     weight_filler {\n\
-      type: \"xavier\"\n\
+      type: \"gaussian\"\n\
+      std: 0.01\n\
     }\n\
     bias_filler {\n\
       type: \"constant\"\n\
@@ -196,17 +136,6 @@ def generate_fc_text(layer_id, num_output, num_conv):
   }\n\
 }\n"
     return text            
-
-
-def generate_relu_text(layer_id):
-    text = "layer {\n\
-  name: \"relu" + str(layer_id) + "\"\n\
-  type: \"ReLU\"\n\
-  bottom: \"ip" + str(layer_id) + "\"\n\
-  top: \"ip" + str(layer_id) + "\"\n\
-}\n"
-    return text
-
 
 def generate_fc_end_text (num_fc_layers, num_conv_layers):
     if (num_fc_layers == 0):
@@ -231,80 +160,25 @@ layer {\n\
   top: \"loss\"\n\
  }\n"
     return end_text
-
-
-## Code to generate prototxt files
-num_conv = xrange(10) #no. of convolution layers
-
-#variable number of convolutional layers with kernel size 5
-##for i in xrange(num_trials):
-##    filename = 'lenet_train_test' + str(i) + '.prototxt'
-##    fid = open(filename, 'w')
-##    fid.write(common_text_start)
-##    for j in xrange(i):
-##        conv_text = generate_conv_text(j, 5)
-##        fid.write(conv_text)
-##        pool_text = generate_pool_text(j)
-##        fid.write(pool_text)
-##    common_text_end = generate_conv_end_text(i)
-##    fid.write(common_text_end)
-##    fid.close()
     
 #variable kernel sizes for fixed 2 convolutional layers
-num_conv_layers = 2
+num_conv_layers = 3
 num_classes = 10
 ker_size = 5
-##kernel_size_list = [3, 5]
-##for k_size in kernel_size_list:
-##    filename = path + 'lenet_train_test_kersize' + str(k_size) + '.prototxt'
-##    fid = open(filename, 'w')
-##    fid.write(common_text_start)
-##    for j in xrange(num_layers):
-##        conv_text = generate_conv_text(j, k_size)
-##        fid.write(conv_text)
-##        pool_text = generate_pool_text(j)
-##        fid.write(pool_text)
-##    common_text_end = generate_conv_end_text(num_layers)
-##    fid.write(common_text_end)
-##    fid.close()
-
-num_fc_layer = 2
+num_fc_layer = 1
 num_fc_neurons = 500
-##num_fc_layer = [1, 2, 3] #num FC layers
-##num_fc_neurons = [100, 300, 500] #size of FC layer
-##for num_fcl in num_fc_layer:
-##    for num_fcn in num_fc_neurons:
-##        filename = path + 'lenet_train_test_fcl' + str(num_fcl) + '_fcn' + str(num_fcn) + '.prototxt'
-##        fid = open(filename, 'w')
-##        fid.write(common_text_start)
-##        for j in xrange(num_conv_layers):
-##            conv_text = generate_conv_text(j, ker_size)
-##            fid.write(conv_text)
-##            pool_text = generate_pool_text(j)
-##            fid.write(pool_text)
-##        for k in xrange(num_fcl):
-##            if (k+1 == num_fcl):
-##                fc_text = generate_fc_text(k+1, num_classes, num_conv_layers)
-##                fid.write(fc_text)
-##            else:
-##                fc_text = generate_fc_text(k+1, num_fcn, num_conv_layers)
-##                fid.write(fc_text)
-##            relu_text = generate_relu_text(k+1)
-##            fid.write(relu_text)
-##        end_text = generate_fc_end_text(num_fcl,num_conv_layers)
-##        fid.write(end_text)
-##        fid.close()
-
 
 #variable number of convolutional features
 feature_size_list = [10, 25, 50, 100, 150]
 for f_size in feature_size_list:
-    filename = path + 'cifarlenet_train_test_featuresize' + str(f_size) + '.prototxt'
+    filename = path + 'cifar_train_test_featuresize' + str(f_size) + '.prototxt'
     fid = open(filename, 'w')
     fid.write(common_text_start)
     for j in xrange(num_conv_layers):
         conv_text = generate_conv_text(j, ker_size, f_size)
         fid.write(conv_text)
+        sigmoid_text = generate_sigmoid_text(j)
+        fid.write(sigmoid_text)
         pool_text = generate_pool_text(j)
         fid.write(pool_text)
     for k in xrange(num_fc_layer):
@@ -314,9 +188,8 @@ for f_size in feature_size_list:
        else:
            fc_text = generate_fc_text(k+1, num_fc_neurons, num_conv_layers)
            fid.write(fc_text)
-       relu_text = generate_relu_text(k+1)
-       fid.write(relu_text)
-    
+           relu_text = generate_relu_text(k+1)
+           fid.write(relu_text)
     common_text_end = generate_fc_end_text(num_fc_layer, num_conv_layers)
     fid.write(common_text_end)
     fid.close()
--- cifar/create_solver_prototxt.py
+++ cifar/create_solver_prototxt.py
@@ -1,6 +1,6 @@
 # Using CIFAR10 training solver from examples - cifar10_full_sigmoid_solver.prototxt
-path = "ECE595/cifar/train_test/var_num_feature_maxiter6000/"
-solver_path = "./solver/var_num_feature_maxiter6000_lr0.01/"
+path = "ECE595/cifar/train_test/var_num_feature_maxiter20000/"
+solver_path = "./solver/var_num_feature_maxiter20000/"
 
 # 10000 iterations --> 10 epoch (64*1000=64000 CIFAR images)
 # writing the solver prototxt
@@ -21,7 +21,7 @@ stepsize: 5000\n\
 # Display every 100 iterations\n\
 display: 100\n\
 # The maximum number of iterations\n\
-max_iter: 6000\n\
+max_iter: 20000\n\
 # snapshot intermediate results\n\
 snapshot: 10000\n\
 snapshot_prefix: \"examples/cifar10_full_sigmoid\"\n\
@@ -30,12 +30,12 @@ solver_mode: GPU"
 
 feature_size_list = [10, 25, 50, 100, 150]
 for f_size in feature_size_list:
-    filename = solver_path + 'cifarlenet_solver_featuresize' + str(f_size) + '.prototxt'
+    filename = solver_path + 'cifar_solver_featuresize' + str(f_size) + '.prototxt'
     fid = open(filename, 'w')
     fid.write("# reduce learning rate after 120 epochs (60000 iters) by factor 0f 10\n\
 # then another factor of 10 after 10 more epochs (5000 iters)\n\n")
     fid.write("# The train/test net protocol buffer definition\n")
-    network_filename = "net: \"" + path + 'cifarlenet_train_test_featuresize' + str(f_size) + ".prototxt\"\n"
+    network_filename = "net: \"" + path + 'cifar_train_test_featuresize' + str(f_size) + ".prototxt\"\n"
     fid.write(network_filename)
     fid.write(common_text)
     fid.close()
