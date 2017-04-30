c6cc9d8e65d37824f92c9dd7bdffdc5bf9f4b0fb
AAYUSH ANKIT <aankit@purdue.edu>
2017-04-30 16:07:32 -0400

    Exhaustive search for CIFAR - batched training scripts

--- cifar/train_cifarlenet_trials_var_featuresize.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh 
-set -e
-./build/tools/caffe train --solver=ECE595/cifar/solver/var_num_feature_maxiter20000/cifar_solver_featuresize10.prototxt $@ 2>&1 | tee ECE595/cifar/traces/var_num_feature_maxiter20000/solver_featuresize10_trace.txt
-./build/tools/caffe train --solver=ECE595/cifar/solver/var_num_feature_maxiter20000/cifar_solver_featuresize25.prototxt $@ 2>&1 | tee ECE595/cifar/traces/var_num_feature_maxiter20000/solver_featuresize25_trace.txt
-./build/tools/caffe train --solver=ECE595/cifar/solver/var_num_feature_maxiter20000/cifar_solver_featuresize50.prototxt $@ 2>&1 | tee ECE595/cifar/traces/var_num_feature_maxiter20000/solver_featuresize50_trace.txt
-./build/tools/caffe train --solver=ECE595/cifar/solver/var_num_feature_maxiter20000/cifar_solver_featuresize100.prototxt $@ 2>&1 | tee ECE595/cifar/traces/var_num_feature_maxiter20000/solver_featuresize100_trace.txt
-./build/tools/caffe train --solver=ECE595/cifar/solver/var_num_feature_maxiter20000/cifar_solver_featuresize150.prototxt $@ 2>&1 | tee ECE595/cifar/traces/var_num_feature_maxiter20000/solver_featuresize150_trace.txt
