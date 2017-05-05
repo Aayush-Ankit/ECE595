e645ff31a0d095fd684aad5214865f0ac857c43f
Sanchari Sen <sen9@descartes.ecn.purdue.edu>
2017-05-05 00:23:06 -0400

    Added pareto optimal solutions

--- mnist/extract_pareto_optimal.py
+++ mnist/extract_pareto_optimal.py
@@ -14,6 +14,11 @@ y = [] #list of hardware optimal configurations
 y1 = [] #list of pareto optimal configurations
 y2 = [] #list of number of convolutional layers for pareto optimal configurations
 y3 = [] # list of cost for pareto optimal configuration
+num_conv = [] # list of number of convolutional layers for pareto optimal
+num_fcl = [] # list of number of fully connected neurons
+num_features0 = []
+num_features1 = []
+
 index = 0
 
 for i in range(len(ds)):
@@ -36,7 +41,19 @@ for i in range(len(y)):
         y1.append(y[i])
         acc2.append(y[i]['accuracy'] * 100)
         y3.append(y[i]['total_comp_cost'])
+
+        num_fcl.append(y[i]['num_fcl'])
+        num_conv.append(y[i]['num_conv'])
+        if (len(y[i]['feature_list']) > 0):
+            num_features0.append(y[i]['feature_list'][0])
+        else:
+            num_features0.append(0)
+        if (len(y[i]['feature_list']) > 1):
+            num_features1.append(y[i]['feature_list'][1])
+        else:
+            num_features1.append(0)
         
+np.save('pareto_optimal.npy', y1)
 
 plt.scatter(acc2, y3, color='red', label = 'pareto optimal total_comp_cost')
 plt.ylim([0,200000])
@@ -49,3 +66,48 @@ plt.title ("pareto optimal computational cost versus Accuracy")
 plt.savefig('pareto_optimal.png')
 
 print("Original number of elements", len(ds), " hardware optimal points with unique accuracies ", len(y), " pareto optimal elements ", len(y1))
+
+plt.figure()
+plt.scatter(acc2, num_conv, label = 'num_conv')
+plt.ylim([0,3])
+plt.xlim([90,100])
+plt.xlabel("Accuracy (%)")
+plt.legend(loc = 'upper left')
+plt.ylabel("number of convolutional layers")
+plt.title ("pareto optimal number of convolutional layers versus Accuracy")
+#plt.show()
+plt.savefig('pareto_optimal_numconv.png')
+
+plt.figure()
+plt.scatter(acc2, num_fcl, label = 'num_fcl')
+plt.ylim([0,4])
+plt.xlim([90,100])
+plt.xlabel("Accuracy (%)")
+plt.legend(loc = 'upper left')
+plt.ylabel("number of fully connected layers")
+plt.title ("pareto optimal number of fully connected layers versus Accuracy")
+#plt.show()
+plt.savefig('pareto_optimal_num_fcl.png')
+
+plt.figure()
+plt.scatter(acc2, num_features0, label = 'num_features[0]')
+plt.ylim([0,100])
+plt.xlim([90,100])
+plt.xlabel("Accuracy (%)")
+plt.legend(loc = 'upper left')
+plt.ylabel("number of features in conv[0]")
+plt.title ("pareto optimal number of features in conv[0] versus Accuracy")
+#plt.show()
+plt.savefig('pareto_optimal_num_features0.png')
+
+plt.figure()
+plt.scatter(acc2, num_features1, label = 'num_features[1]')
+plt.ylim([0,100])
+plt.xlim([90,100])
+plt.xlabel("Accuracy (%)")
+plt.legend(loc = 'upper left')
+plt.ylabel("number of features in conv[1]")
+plt.title ("pareto optimal number of features in conv[1] versus Accuracy")
+#plt.show()
+plt.savefig('pareto_optimal_num_features1.png')
+
